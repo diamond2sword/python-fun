d3a57eb7715d47450f483163f3e394a1bcabad86
diamond2sword <diamond2sword@gmail.com>
2023-01-13 21:16:28 +0800

    update project

---
+++ import-ifs/main.py
@@ -0,0 +1,8 @@
+
+def main():
+    run.main()
+    print
+
+import run
+if __name__ == "__main__":
+    main()
---
+++ import-ifs/run.py
@@ -0,0 +1,10 @@
+def main():
+    say_hello_world()
+
+
+def say_hello_world():
+    print("Hello World!")
+
+
+if __name__ == "__main__":
+    main()
