5b095daac15d0548f724491710a140fa9b7bdf59
diamond2sword <diamond2sword@gmail.com>
2023-08-03 11:21:06 +0800

    update project

--- fogchess/fogchess.py
+++ fogchess/fogchess.py
@@ -1,15 +1,16 @@
-from typing import TypedDict, cast
+import typing
+from copy import deepcopy, copy
 
 def main()->None:
     a: Piece= Piece([1],[3],[{"rank": [4],"file":[0]}],[],[],[],[])
     b: Piece= Piece([0,1],[],[{"rank": [1,2,3,5,6,7],"file":[0]}],[],[],[],[])
     print(a.satisfies(b))
-    print(b.varDict)
+    print(b.__dict__)
 
     B: Board= Board(8, 0)
-    B.pieces[0][0].varDict["name"]= [1]
-    B.pieces[1][0].varDict["name"]= [2]
-    B.spawn(a)
+    B.pieces[0][0].name= [1]
+    B.pieces[1][0].name= [2]
+    B.spawn([a])
     print(B)
     pass
 
@@ -21,20 +22,17 @@ class Move:
         @staticmethod
         def expand(moveA: "Move", boardA: "Board")->list["Move"]:
             resultMoves= []
-            moveB: Move= moveA
-            for rank,file,pieceA in boardA:
-                if not pieceA.satisfies(moveA.varDict["oldPiece"]): continue
-                moveB.varDict["oldPiece"]= pieceA
-                boardB= boardA
-                for iteration in range(moveA.varDict["radius"]):
+            moveB: Move= deepcopy(moveA)
+            for rank, file, pieceA in boardA:
+                if not pieceA.satisfies(moveA.oldPiece): continue
+                moveB.oldPiece= pieceA
+                boardB= deepcopy(boardA)
+                for iteration in range(moveA.radius):
                     if not boardB.satisfies(moveA):
                         break
-                    moveB.varDict["iteration"]= iteration+1
-                    resultMoves.append(moveB)
-                    for pieceC in moveA.varDict["changes"]:
-                        boardB.spawn(pieceC)
-
-                         
+                    moveB.iteration= iteration+1
+                    resultMoves.append(deepcopy(moveB))
+                    boardB.spawn(moveA.changes) 
             return resultMoves
 
     def __init__(self,
@@ -46,29 +44,20 @@ class Move:
                  changes: list["Piece"],
                  conditions: list["Piece"],
                  moveParams: list["Move.Param"],)->None:
-        self.varDict: Move.VarDict= {"name": name,
-                                     "radius": radius,
-                                     "iteration": iteration,
-                                     "oldPiece": oldPiece,
-                                     "newPiece": newPiece,
-                                     "changes": changes,
-                                     "conditions": conditions,
-                                     "moveParams": moveParams,}
+        self.name= name
+        self.radius= radius
+        self.iteration= iteration
+        self.oldPiece= oldPiece
+        self.newPiece= newPiece
+        self.changes= changes
+        self.conditions= conditions
+        self.moveParams= moveParams
         if iteration != 0:
-            self.defDict: dict[str,int]= {"x": int(oldPiece.varDict["coordinates"][0]["file"][0]),
-                                          "y": int(oldPiece.varDict["coordinates"][0]["rank"][0]),}
+            self.defDict: dict[str,int]= {"x": int(oldPiece.coordinates[0]["file"][0]),
+                                          "y": int(oldPiece.coordinates[0]["rank"][0]),}
             
 
    
-    class VarDict(TypedDict):
-        name: int
-        radius: int
-        iteration: int
-        oldPiece: "Piece"
-        newPiece: "Piece"
-        changes: list["Piece"]
-        conditions: list["Piece"]
-        moveParams: list["Move.Param"]
 
     class Param:
         @staticmethod
@@ -94,13 +83,14 @@ class Move:
 
                 
 class Board:
-    def spawn(self, piece: "Piece")->None:
-        c= piece.varDict["coordinates"][0]
-        self.pieces[int(c["file"][0])][int(c["rank"][0])]= piece
+    def spawn(self, changes: list["Piece"])->None:
+        for piece in changes:
+            c= piece.coordinates[0]
+            self.pieces[int(c["file"][0])][int(c["rank"][0])]= deepcopy(piece)
 
     def satisfies(self, move: Move)->bool: 
-        for pieceB in move.varDict["conditions"]:
-            c= pieceB.varDict["coordinates"][0]
+        for pieceB in move.conditions:
+            c= pieceB.coordinates[0]
             pieceA= self.pieces[int(c["file"][0])][int(c["rank"][0])]
             if pieceA.satisfies(pieceB):
                 continue
@@ -111,7 +101,7 @@ class Board:
         text: str=""
         for i in range(self.width):
             for j in range(self.width):
-                text+= str(self.pieces[j][self.width-1-i].varDict["name"])
+                text+= str(self.pieces[j][self.width-1-i].name)
                 text+= ","
             text+= "\n"
         return text
@@ -125,8 +115,8 @@ class Board:
             self.pieces[i]= {}
             for j in range(self.width):
                 self.pieces[i][j]= Piece.EMPTY()
-                self.pieces[i][j].varDict["coordinates"]= [{"rank": [j],
-                                                            "file": [i],}]
+                self.pieces[i][j].coordinates= [{"rank": [j],
+                                                 "file": [i],}]
 
     def __iter__(self)->"Board":
         self.iterN: int= 0
@@ -137,8 +127,8 @@ class Board:
         if self.iterN == self.iterMax:
             raise StopIteration
         self.iterN+= 1
-        file= self.iterN % self.width
-        rank= self.iterN // self.width
+        file: int= self.iterN % self.width
+        rank: int= self.iterN // self.width
         return (file, rank, self.pieces[file][rank])
 
 
@@ -149,10 +139,10 @@ class Board:
 
 class Piece:
     def satisfies(self, other: "Piece")->bool:
-        for k in self.varDict.keys():
-            if other.varDict[k] == []: #type: ignore[literal-required]
+        for k in self.__dict__.keys():
+            if other.__dict__[k] == []:
                 continue
-            if self.varDict[k][0] in other.varDict[k]: #type: ignore[literal-required]
+            if self.__dict__[k][0] in other.__dict__[k]:
                 continue
             return False
         return True
@@ -175,32 +165,23 @@ class Piece:
                  blackLightLevel: list[int], 
                  recentMove: list[dict[str,list[int]]], 
                  hasMoved: list[bool],)->None:
-        self.varDict: Piece.VarDict= {"name": name,
-                                      "team": team,
-                                      "coordinates": [],
-                                      "whiteLightLevel": whiteLightLevel,
-                                      "blackLightLevel": blackLightLevel,
-                                      "recentMove": [],
-                                      "hasMoved": hasMoved,}
+        self.name= name
+        self.team= team
+        self.coordinates= []
+        self.whiteLightLevel= whiteLightLevel
+        self.blackLightLevel= blackLightLevel
+        self.recentMove= []
+        self.hasMoved= hasMoved
         for cDict in coordinates:
             for r in cDict["rank"]:
                 for f in cDict["file"]:
-                    self.varDict["coordinates"].append({"rank": [r],
-                                                        "file": [f]})
+                    self.coordinates.append({"rank": [r],
+                                             "file": [f]})
         for rDict in recentMove:
             for n in rDict["name"]:
                 for i in rDict["iteration"]:
-                    self.varDict["recentMove"].append({"name": [n],
-                                                       "iteration": [i]})
-
-    class VarDict(TypedDict):
-        name: list[int|str]
-        team: list[int]
-        coordinates: list[dict[str,list[int|str]]]
-        whiteLightLevel: list[int]
-        blackLightLevel: list[int] 
-        recentMove: list[dict[str,list[int]]]
-        hasMoved: list[bool]
+                    self.recentMove.append({"name": [n],
+                                            "iteration": [i]})
 
 
 
