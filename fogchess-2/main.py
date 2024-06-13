ee7532328bce9ba356411f626868279258c06f59
diamond2sword <diamond2sword@gmail.com>
2024-06-13 11:49:17 +0800

    update project

---
+++ fogchess-2/main.py
@@ -0,0 +1,13 @@
+from typing import Callable, Optional, List, Any, Dict
+from __future__ import annotations
+
+def main() -> int:
+    print("Hello World!")
+    return 0
+
+class Game:
+    def __init__(self):
+    pass
+
+if __name__ == "__main__":
+    main()
